7a296e916e143ee8f931041542ffd81a17cad769
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-04-02 14:24:49 +0200

    init

---
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-home',
+  standalone: false,
+  templateUrl: './collections-home.component.html',
+  styleUrl: './collections-home.component.css',
+})
+export class CollectionsHomeComponent {}
---
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-home',
+  standalone: false,
+  templateUrl: './elements-home.component.html',
+  styleUrl: './elements-home.component.css',
+})
+export class ElementsHomeComponent {}
